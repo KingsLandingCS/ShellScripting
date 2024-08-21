174263d54105ad232b4cb4eed5c69954ac8fc692
KingsLandingCS <finalfantasy.fantasy9@gmail.com>
2024-08-21 20:21:32 +0500

    learning

--- hello.sh
+++ hello.sh
@@ -1,2 +1,15 @@
 #! /usr/bin/bash
+# this is a comment
+
 echo "Hello, World!"
+
+echo Our shell name is $BASH
+echo Our shell version is $BASH_VERSION
+echo Our home directory is $HOME
+echo Our present/print working directory $PWD
+
+name=Mark
+val=10
+
+echo the name is $name
+echo value $val

dc5a2d3c438042fa8566f7970f8d20221131d99a
KingsLandingCS <finalfantasy.fantasy9@gmail.com>
2024-08-23 23:34:45 +0500

    learning

---
+++ BASH-2.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+# echo "Enter name: "
+# read name1 name2 name3
+# echo "Names : $name1, $name2, $name3"
+
+read -p "username: " userVar
+read -sp "password: " passVar
+echo
+echo "username: $userVar"
+echo "password: $passVar"

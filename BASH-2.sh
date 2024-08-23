e413d91c3a4bbc955ebcf727ade993e0ab0a8cfa
KingsLandingCS <finalfantasy.fantasy9@gmail.com>
2024-08-23 23:41:02 +0500

    learning

--- BASH-2.sh
+++ BASH-2.sh
@@ -4,8 +4,12 @@
 # read name1 name2 name3
 # echo "Names : $name1, $name2, $name3"
 
-read -p "username: " userVar
-read -sp "password: " passVar
-echo
-echo "username: $userVar"
-echo "password: $passVar"
+# read -p "username: " userVar
+# read -sp "password: " passVar
+# echo
+# echo "username: $userVar"
+# echo "password: $passVar"
+
+echo "Enter names: "
+read -a names
+echo "Names: ${names[0]} ${names[1]} ${names[2]}"

078eb36715cff82cd87833ec4802c0e93b72c65a
KingsLandingCS <finalfantasy.fantasy9@gmail.com>
2024-08-27 01:43:46 +0500

    learning

--- BASH-2.sh
+++ BASH-2.sh
@@ -13,3 +13,5 @@
 echo "Enter names: "
 read -a names
 echo "Names: ${names[0]} ${names[1]} ${names[2]}"
+
+
---
+++ argument.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#echo $0 $1 $2 $3    ' > echo $1 $2 $3'
+
+# Echo positional arguments
+echo "$0" "$1" "$2" "$3"
+
+# Store all arguments in an array
+args=("$@")
+
+# Access and print array elements
+echo "${args[0]}" "${args[1]}" "${args[2]}"

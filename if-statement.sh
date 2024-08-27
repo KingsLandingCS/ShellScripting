7c93d10a5e8e8ca5e766f9465b2abc30a87a9691
KingsLandingCS <finalfantasy.fantasy9@gmail.com>
2024-08-28 03:50:56 +0500

    learning

--- if-statement.sh
+++ if-statement.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
 # Example of an if statement in bash
-echo "Enter a number:"
-read number
+# echo "Enter a number:"
+# read number
 
-if [ "$number" -ne 10 ]; then
-    echo "The number is greater than 10."
+# if [ "$number" -ne 10 ]; then
+#     echo "The number is greater than 10."
+# else
+#     echo "The number is 10 or less."
+# fi
+
+word=abc
+
+if [ "$word" = "abcava" ]; then
+    echo "condition is true"
 else
-    echo "The number is 10 or less."
+    echo "condition is false."
 fi

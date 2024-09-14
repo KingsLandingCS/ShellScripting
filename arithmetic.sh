b1234cbc5f1810fde238a64d4d3dd8584dfa00d9
KingsLandingCS <finalfantasy.fantasy9@gmail.com>
2024-09-14 07:56:29 +0500

    learning

--- arithmetic.sh
+++ arithmetic.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
-num1=20
+num1=20.5
 num2=50
 
-echo $(( num1 + num2 ))
-echo $(( num1 - num2 ))
-echo $(( num1 * num2 ))
-echo $(( num1 / num2 ))
-echo $(( num1 % num2 ))
+echo "20.5+5" | bc
+echo "20.5-5" | bc
+echo "20.5*5" | bc
+echo "scale=2; 20.5/5" | bc
+echo "20.5%5" | bc
